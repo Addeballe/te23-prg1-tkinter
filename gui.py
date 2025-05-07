7fd16610d101338045b581307b1e5c3a972fc29e
Addeballe <adrian.nilsson@elev.ga.ntig.se>
2025-05-07 08:58:12 +0200

    gui

--- gui.py
+++ gui.py
@@ -2,12 +2,11 @@ from tkinter import *
 from random import randint
 
 root = Tk()
-w = Label(root, text='Program that fucking kills itself!')
+root.title('Program that nicely kills itself')
+w = Label(root, text='Program that *nicely* kills itself!')
 w.pack()
 
 kill_button = Button(root, text='Kill Button', width=30, height=5, command=root.destroy)
 kill_button.pack()
-
-
 
 root.mainloop()
